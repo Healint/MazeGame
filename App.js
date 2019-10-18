18528338dc54ec8f3eda54ab86fb24450f977fe2
zeeshan tufail <zeeshan@healint.com>
2019-10-18 19:52:14 +0800

    enabled debugging somehow

--- App.js
+++ App.js
@@ -1,22 +1,61 @@
 import React, {Component} from 'react';
 import {SafeAreaView, View, FlatList, StyleSheet, Text} from 'react-native';
 
-import {MazeActionProcessor} from './MazeCommunication/MazeActionProcessor';
+const DATA1 = {
+  id: '1',
+  data: [
+    {
+      id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+      title: 'First Item',
+    },
+    {
+      id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
+      title: 'Second Item',
+    },
+    {
+      id: '58694a0f-3da1-471f-bd96-145571e29d72',
+      title: 'Third Item',
+    },
+  ],
+};
 
-const DATA = [
-  {
-    id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
-    title: 'First Item',
-  },
-  {
-    id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
-    title: 'Second Item',
-  },
-  {
-    id: '58694a0f-3da1-471f-bd96-145571e29d72',
-    title: 'Third Item',
-  },
-];
+const DATA2 = {
+  id: '2',
+  data: [
+    {
+      id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+      title: 'First Item',
+    },
+    {
+      id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
+      title: 'Second Item',
+    },
+    {
+      id: '58694a0f-3da1-471f-bd96-145571e29d72',
+      title: 'Third Item',
+    },
+  ],
+};
+
+const DATA3 = {
+  id: '3',
+  data: [
+    {
+      id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+      title: 'First Item',
+    },
+    {
+      id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
+      title: 'Second Item',
+    },
+    {
+      id: '58694a0f-3da1-471f-bd96-145571e29d72',
+      title: 'Third Item',
+    },
+  ],
+};
+
+const DATA = [DATA1, DATA2, DATA3];
 
 function Item({title}) {
   return (
@@ -47,10 +86,8 @@ export default class HelloWorldApp extends Component {
       uri:
         'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg',
     };
-    let actionProcessor = new MazeActionProcessor();
-    actionProcessor.startMaze();
     let item = this.getItem;
-    let flatlistColumn = this.getFlatList;
+    let flatlistColumn = this.getFlatListTamp;
     return (
       <SafeAreaView style={styles.container}>
         {flatlistColumn(item)}
@@ -63,20 +100,37 @@ export default class HelloWorldApp extends Component {
       <FlatList
         horizontal={true}
         data={DATA}
-        renderItem={({item}) => (
+        renderItem={({datar}) => (
           <FlatList
-            horizontal={true}
-            data={DATA}
-            renderItem={({item}) => renderItem(item)}
-            keyExtractor={item => item.id}
+            horizontal={false}
+            data={datar.data}
+            renderItem={({datac}) => renderItem(datac)}
+            keyExtractor={datac => datac.id}
           />
         )}
+        keyExtractor={datar => datar.id}
+      />
+    );
+  }
+
+  getFlatListTamp(renderItem) {
+    console.log(DATA[0].data[0].title);
+    return (
+      <FlatList
+        horizontal={false}
+        data={DATA[0].data}
+        renderItem={datac => renderItem(datac.item)}
         keyExtractor={item => item.id}
       />
     );
   }
 
+  getId(datac) {
+    return datac.id;
+  }
+
   getItem(item) {
+    console.log('item id: ' + item.id);
     return <Item title={item.title} />;
   }
 }
