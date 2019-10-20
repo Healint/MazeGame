287beca4234b5fa83ea58c2c322c8004e137eeec
Nicolas Paris <nicolas.paris@healint.com>
2019-10-20 10:53:33 +0800

    Hurdle and Loot generation improved: 1. Can't be created on walls (>...<) 2. Loot more likely in rooms 3. Hurdle more likely in corridors

--- MazeBackend/Maze/Cell.js
+++ MazeBackend/Maze/Cell.js
@@ -18,22 +18,44 @@ export class Cell {
     return `Cell(x=${this.x},y=${this.y},actor=${this.actor})`;
   }
 
+  roll_actor(floor) {
+    // customize probability of rolling actors depending on the type of floor
+    // if we implement difficulty scaling, it should be there
+    let rng = Math.random();
+    let extra_proba_hurdle = 0;
+    let extra_proba_loot = 0;
+    switch (floor) {
+      case 1:
+        // extra chance of Loot in rooms
+        extra_proba_hurdle = 0;
+        extra_proba_loot = 0.04;
+        break;
+      case 2:
+        // extra chance of Hurdle in corridors
+        extra_proba_hurdle = 0.07;
+        extra_proba_loot = 0;
+        break;
+    }
+
+    if (rng > UNIVERSE_CONSTANTS.hurdle_proba + extra_proba_hurdle) {
+      return HurdleFactory();
+    } else if (rng > UNIVERSE_CONSTANTS.loot_proba + extra_proba_loot) {
+      return LootFactory();
+    }
+  }
+
   constructor(x: null, y: null, floor: null, actor: null) {
     this.x = x;
     this.y = y;
     this.floor = floor;
     this.actor = actor;
 
-    let rng = Math.random();
-
-    // generate all actors that are not the player
-    if (rng < UNIVERSE_CONSTANTS.hurdle_proba) {
-      this.actor = HurdleFactory();
-    } else if (
-      rng <
-      UNIVERSE_CONSTANTS.hurdle_proba + UNIVERSE_CONSTANTS.loot_proba
-    ) {
-      this.actor = LootFactory();
+    // if uncarveable, we don't roll for hurdles or loot
+    if (floor === 0 || floor === 7) {
+      // uncarveable, skip
+    } else {
+      // first we roll for a hurdle
+      this.actor = this.roll_actor(floor);
     }
   }
 
